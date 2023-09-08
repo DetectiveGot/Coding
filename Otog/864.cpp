04158a3008d3ccf16595b53d406863ca618893e1
DetectiveGot <72302631+DetectiveGot@users.noreply.github.com>
2023-09-08 14:59:51 +0700

    864.cpp

--- Otog/864.cpp
+++ Otog/864.cpp
@@ -1,61 +1,11 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define pii pair<int,int>
+
 using namespace std;
-using pii = pair<int, int>;
 
-const int N = 100005;
-
-int n, k, q, x;
-int e[N], dist[N];
-priority_queue<pii> pq;
-vector<int> adj[N];
-
-int main()
-{
-    scanf("%d %d %d %d", &n, &k, &q, &x);
-
-    for (int i = 1; i <= n; i++)
-    {
-        scanf("%d", &e[i]);
-    }
-
-    int a, b;
-    for (int i = 0; i < k; i++)
-    {
-        scanf("%d%d", &a, &b);
-        adj[a].push_back(b);
-    }
-
-    // fill(dist, dist + n + 1, INT_MAX);
-    for (int i = 0; i <= n; i++) dist[i] = INT_MAX;
-    dist[x] = e[x];
-    pq.push({dist[x], x});
-
-    while (not pq.empty())
-    {
-        int now = pq.top().second;
-        int w = pq.top().first;
-        pq.pop();
-
-        if (dist[now] < w)
-            continue;
-
-        for (auto child : adj[now])
-        {
-            int nx = child;
-
-            if (dist[nx] > dist[now] + e[nx])
-            {
-                dist[nx] = dist[now] + e[nx];
-                pq.push({dist[nx], nx});
-            }
-        }
-    }
-
-    while (q--)
-    {
-        scanf("%d", &a);
-        printf("ans: %d\n", dist[a]);
-    }
-
+int main(){
+    cin.tie(nullptpr)->sync_with_stdio(0);
+    
     return 0;
 }
