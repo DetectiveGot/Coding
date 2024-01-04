25d33a42d0df4dca6a7b5efad73f8f10e6132018
DetectiveGot <72302631+DetectiveGot@users.noreply.github.com>
2024-01-04 11:20:10 +0700

    Add files via upload

---
+++ Template/Normal.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pii pair<int,int>
+#define deb(x) cerr << "[" << #x << ":" << x << "] "
+
+using namespace std;
+
+int main(){
+	cin.tie(nullptr)->sync_with_stdio(0);
+	
+	
+	return 0;
+}
---
+++ Template/OrderedSet.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+#define ll long long
+#define pii pair<int,int>
+#define deb(x) cerr << "[" << #x << ":" << x << "] "
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main(){
+	cin.tie(nullptr)->sync_with_stdio(0);
+	
+	
+	return 0;
+}
