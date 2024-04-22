cd5a4b8dc7f7a74d0c64f3002ee0a0de7b6e5fe5
Paphangkon Praphuttiphong <72302631+DetectiveGot@users.noreply.github.com>
2024-04-22 20:05:43 +0700

    Update Normal.cpp

--- Template/Normal.cpp
+++ Template/Normal.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 #define ll long long
 #define pii pair<int,int>
+#define tpi tuple<int,int,int>
+#define siz(x) (int)(x.size())
 #define deb(x) cerr << "[" << #x << ": " << x << "] "
 
 using namespace std;
@@ -8,6 +10,6 @@ using namespace std;
 int main(){
 	cin.tie(nullptr)->sync_with_stdio(0);
 	
-	
+
 	return 0;
 }
